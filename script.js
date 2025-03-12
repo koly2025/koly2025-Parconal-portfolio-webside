c952a52ceb46c9211fcae377b5edcbd487ffd85e
koly begum <koly.ict25@gmail.com>
2025-03-13 03:26:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Optional JavaScript for button interaction
+document.getElementById('hire-btn').addEventListener('click', function() {
+  alert("Thank you for your interest! I will get back to you soon.");
+});
